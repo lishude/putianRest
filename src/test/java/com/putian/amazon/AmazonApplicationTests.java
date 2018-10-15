719c79056654af905da9aa3f8787c6500ef1a441
李书德 <117799801@qq.com>
2018-10-15 15:33:02 +0800

    my first commit

---
+++ src/test/java/com/putian/amazon/AmazonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.putian.amazon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AmazonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
